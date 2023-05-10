73447e74f99e94a0bd5cb07677e6b65115670f00
Simonas Karpavičius <karpavicius.simonas@gmail.com>
2023-05-10 16:58:36 +0300

    Shooting Script Adjustment

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -4,38 +4,48 @@ public class Gun : MonoBehaviour
 {
     public GameObject hitPrefab; //bullet hole
     public GameObject mFlashPrefab; //muzzle flash
-    public float mFlashDuration = 0.5f;
-    public float mFlashTime;
-    public AudioSource shot;
     public float maxDistance = 100;
+    
+    public AudioClip shotSound;
+    AudioSource audio;
 
     private void Start()
     {
-        mFlashPrefab.SetActive(false);
+        audio = gameObject.AddComponent<AudioSource>();
+        audio.volume = 0.1f;
     }
 
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
-            var ray = new Ray(transform.position, transform.forward);
-            mFlashPrefab.SetActive(true);
-            shot.Play();
-            mFlashTime = Time.time + mFlashDuration;
-
-            if (Physics.Raycast(ray, out var hit, maxDistance))
-            {
-                //hit
-                print(hit.point);
-                var hitObj = Instantiate(hitPrefab, hit.point, Quaternion.Euler(0, 0, 0));
-                hitObj.transform.forward = hit.normal;
-                hitObj.transform.position += hit.normal * 0.01f;
-            }
+            Shoot();
         }
+    }
 
-        if(mFlashPrefab.activeSelf && mFlashTime <= Time.time)
+    void Shoot()
+    {
+        var cam = Camera.main;
+        var ray = new Ray(cam.transform.position, cam.transform.forward);
+
+        mFlashPrefab.SetActive(true);
+        Invoke("DisableFlash", 0.05f);
+
+        audio.pitch = Random.Range(0.85f, 1.15f);
+        audio.PlayOneShot(shotSound);
+
+        if (Physics.Raycast(ray, out var hit, maxDistance))
         {
-            mFlashPrefab.SetActive(false);
+            //hit
+            print(hit.point);
+            var hitObj = Instantiate(hitPrefab, hit.point, Quaternion.Euler(0, 0, 0));
+            hitObj.transform.forward = hit.normal;
+            hitObj.transform.position += hit.normal * 0.01f;
         }
+    }
+
+    void DisableFlash()
+    {
+        mFlashPrefab.SetActive(false);
     }
 }
